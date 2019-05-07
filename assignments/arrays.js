0eba4262b9ba55a8d510ee1490b0def8bbc1a945
Hoxtygen <hoxtygen@live.com>
2019-05-07 07:54:01 +0100

    refactor codes in array file
    The changes made here are the following
    1. Challenge 6 in arrays file was changed to use Array.map() function. Array.filter() function will also work here too.
    2. Challenge 5 was refactored to get it's data from the carYears variable rather than the inventory array.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -85,7 +85,7 @@ inventory.filter(element => {
     return carModels.sort().push(element.car_model);
 })
 console.log(carModels);
-//console.table(carModels);
+console.table(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
@@ -98,20 +98,26 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-inventory.map(oldy => {
-    return oldy.car_year < 2000 ? oldCars.push(oldy.car_year) : "";
+carYears.map(oldy => {
+    return oldy < 2000 ? oldCars.push(oldy) : "";
 })
 console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-for (let i = 0; i < inventory.length; i++) {
-    if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
-        BMWAndAudi.push(inventory[i])
-        //console.table(inventory[i])
+// for (let i = 0; i < inventory.length; i++) {
+//     if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
+//         BMWAndAudi.push(inventory[i])
+//         //console.table(inventory[i])
+//     }
+// }
+inventory.map(item => {
+    if (item.car_make === "BMW" || item.car_make === "Audi") {
+        BMWAndAudi.push(item)
     }
-}
+})
+console.table(BMWAndAudi)
 console.log(JSON.stringify(BMWAndAudi));
 
 
